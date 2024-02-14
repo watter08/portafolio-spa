53a92a5581cfeedb662cd66e7ac973792ce34a7a
watter08 <watter08@hotmail.es>
2024-02-14 19:47:39 -0400

    sass added

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom' 
 import Routes from './router/router.jsx'
-
+import './assets/sass/main.scss'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
