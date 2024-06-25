b4d4e9f097eb55816f913468f5fcc8538ce139b4
watter08 <watter08@hotmail.es>
2024-06-25 16:02:46 -0400

    waves border bottom fixed, embla carousel component created

--- src/Views/HomeView.jsx
+++ src/Views/HomeView.jsx
@@ -1,23 +1,37 @@
 import { useState } from 'react'
-import { Navbar,  ShapeHeader, Presentation, ImageSlide } from '../components/index'
+import { Navbar,  ShapeHeader, Presentation, ImageSlide, EmblaCarousel } from '../components/index'
 import { PresentationInfo } from '../logic/const/presentation.const';
+import { ProjectsWorked } from '../logic/const/projects.const'
+
 function HomeView(){
     const [ currentLenguage, setCurrentLenguage ] = useState('Title');
+    const [Options] = useState({ axis: 'y', dragFree: true, direction: 'rtl', loop: true })
 
     return(
         <div className='container-fluid position-relative p-0 m-0'>
             <div className="container-shape">
                 <Navbar className="position-absolute" setCurrentLenguageParent={setCurrentLenguage} />
-
                 <Presentation 
-                About={PresentationInfo.About} 
-                ImageUrl={PresentationInfo.ImageUrl} 
-                CurrentLanguage={currentLenguage} 
-                key="Presentation" />                
-                {/* <GeneralInfo className="position-absolute" whoIAmData={WhoIAm} currentLanguage={currentLenguage} /> */}
+                    About={PresentationInfo.About} 
+                    ImageUrl={PresentationInfo.ImageUrl} 
+                    CurrentLanguage={currentLenguage} 
+                    key="Presentation" />                   
                 <ShapeHeader />
             </div>  
-            <ImageSlide />          
+            {/* <ImageSlide />       */}
+
+            <div className="row position-relative pt-5 mt-5">
+                <div className="embla-containers">
+                    <EmblaCarousel 
+                    currentLenguaje={currentLenguage} 
+                    slides={ProjectsWorked} 
+                    options={Options} 
+                    key="EmblaCarouselHome"
+                    /> 
+                </div>
+            </div>
+               
+            {/* <GeneralInfo className="position-absolute" whoIAmData={WhoIAm} currentLanguage={currentLenguage} /> */}
         </div>
     )
 }
--- src/components/Sliders/ImageSlide.jsx
+++ src/components/Sliders/ImageSlide.jsx
@@ -1,51 +1,66 @@
-import { useEffect } from "react"; 
-import { gsap } from 'gsap';
+import useEmblaCarousel from 'embla-carousel-react'
+import Autoplay from 'embla-carousel-autoplay'
 
 function ImageSlide() {
-
+  const [emblaRef] = useEmblaCarousel({ loop: false }, [Autoplay()])
     return(
-        <div className="row">
-<div id="carouselExampleCaptions" className="carousel slide">
-  <div className="carousel-indicators">
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" className="active" aria-current="true" aria-label="Slide 1"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
-    <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <div className="row position-relative pt-5 mt-5">
+          <div className="embla-containers">
+            <div className="embla" ref={emblaRef}>
+              <div className="embla__container">
+                <div className="embla__slide">
+                  <h3 className="embla-title">Explode Land Rover</h3>
+                  <h4 className="embla-subtitle">Defender 90</h4>
+                  <h6 className="embla-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing 
+                    elit. Asperiores quas quae corporis qui ducimus 
+                    praesentium, dolor temporibus vel autem voluptatibus, 
+                    modi, consequuntur id velit assumenda quibusdam iure 
+                    alias amet voluptas.
+                  </h6>
+                  <img className="embla-img" src="/src/assets/image/slide/project_1.webp" alt="" />
+                </div>
+                <div className="embla__slide">
+                  <h3 className="embla-title">Explode Land Rover</h3>
+                  <h4 className="embla-subtitle">Defender 90</h4>
+                  <h6 className="embla-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing 
+                    elit. Asperiores quas quae corporis qui ducimus 
+                    praesentium, dolor temporibus vel autem voluptatibus, 
+                    modi, consequuntur id velit assumenda quibusdam iure 
+                    alias amet voluptas.
+                  </h6>
+                  <img className="embla-img" src="/src/assets/image/slide/project_2.webp" alt="" />
+                </div>
+                <div className="embla__slide">
+                  <h3 className="embla-title">Explode Land Rover</h3>
+                  <h4 className="embla-subtitle">Defender 90</h4>
+                  <h6 className="embla-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing 
+                    elit. Asperiores quas quae corporis qui ducimus 
+                    praesentium, dolor temporibus vel autem voluptatibus, 
+                    modi, consequuntur id velit assumenda quibusdam iure 
+                    alias amet voluptas.
+                  </h6>
+                  <img className="embla-img" src="/src/assets/image/slide/project_1.webp" alt="" />
+                </div>
+                <div className="embla__slide">
+                  <h3 className="embla-title">Explode Land Rover</h3>
+                  <h4 className="embla-subtitle">Defender 90</h4>
+                  <h6 className="embla-details">
+                    Lorem, ipsum dolor sit amet consectetur adipisicing 
+                    elit. Asperiores quas quae corporis qui ducimus 
+                    praesentium, dolor temporibus vel autem voluptatibus, 
+                    modi, consequuntur id velit assumenda quibusdam iure 
+                    alias amet voluptas.
+                  </h6>
+                  <img className="embla-img" src="/src/assets/image/slide/project_2.webp" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          
   </div>
-  <div className="carousel-inner">
-    <div className="carousel-item active">
-      <img src="/src/assets/image/img_3.png" className="d-block w-100" alt="/src/assets/image/img_3.png" />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>First slide label</h5>
-        <p>Some representative placeholder content for the first slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="/src/assets/image/img_3.png" className="d-block w-100" alt="/src/assets/image/img_3.png" />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Second slide label</h5>
-        <p>Some representative placeholder content for the second slide.</p>
-      </div>
-    </div>
-    <div className="carousel-item">
-      <img src="/src/assets/image/img_3.png" className="d-block w-100" alt="/src/assets/image/img_3.png" />
-      <div className="carousel-caption d-none d-md-block">
-        <h5>Third slide label</h5>
-        <p>Some representative placeholder content for the third slide.</p>
-      </div>
-    </div>
-  </div>
-  <button className="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
-    <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Previous</span>
-  </button>
-  <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
-    <span className="carousel-control-next-icon" aria-hidden="true"></span>
-    <span className="visually-hidden">Next</span>
-  </button>
-</div>
-        </div>
     )
 }
-
-
 export default ImageSlide
