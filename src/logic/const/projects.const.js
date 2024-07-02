990d7bf878ba2a0f868f572aed8a7813fcaf334b
watter08 <watter08@hotmail.es>
2024-07-01 22:56:04 -0400

    q

--- src/logic/const/projects.const.js
+++ src/logic/const/projects.const.js
@@ -11,7 +11,7 @@ ES:
             Title:'Portafolio',
             SubTitle:'https://vue-coin.vercel.app',
             Details:'Esta aplicación web, desarrollada con ReactJS 18.2.0 y utilizando Vite, está diseñada para proporcionar a los visitantes toda la información relevante para el proceso de reclutamiento de un individuo. La plataforma incluye una presentación personal, un portafolio de proyectos, certificaciones obtenidas en plataformas como HackerRank y SoloLearn, así como una lista detallada de habilidades y competencias. Además, se ofrece una visión completa del perfil del colaborador, incluyendo su experiencia laboral y datos de contacto, todo disponible en dos idiomas: español e inglés.',
-            Image:'/src/assets/image/projects/cripto-monedas.png'
+            Image:'/src/assets/image/projects/portafolio.png'
         }
     ],
 EN:
@@ -26,7 +26,7 @@ EN:
             Title: 'Portfolio',
             SubTitle: 'https://vue-coin.vercel.app',
             Details: 'This web application, developed with ReactJS 18.2.0 and utilizing Vite, is designed to provide visitors with comprehensive information relevant to the recruitment process. The platform includes a personal introduction, a portfolio of projects, certifications from platforms such as HackerRank and SoloLearn, as well as a detailed list of skills and competencies. Additionally, it offers a complete profile of the individual, including work experience and contact information, all available in two languages: Spanish and English.',
-            Image: '/src/assets/image/projects/cripto-monedas.png'
+            Image: '/src/assets/image/projects/portafolio.png'
         }        
     ]
 }
